d5218e241721cc823f0deeda9c43cf6e90a3746c
Eugene 'marengohue' Rebedailo <mokonamodokidesu@gmail.com>
2021-08-02 02:36:06 +0200

    Add persistent xmobar

--- src/home/__usrname__/.xmonad/xmonad.hs
+++ src/home/__usrname__/.xmonad/xmonad.hs
@@ -1,12 +1,19 @@
 import XMonad
+import XMonad.Config.Desktop
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
 
 xmobarRc = "~/.xmonad/xmobar.hs"
-	
-main = do
-	xmproc <- spawnPipe ("xmobar" ++ xmobarRc)
-	xmonad $ defaults
 
-defaults = defaultConfig {
-	terminal	= "alacritty"
-	modMask		= mod4Mask
+customWorkspaces = [ "main", "web", "steam" ] ++ [ "4", "5", "6" ]
+
+main = do
+  xmproc <- spawnPipe ("xmobar " ++ xmobarRc)
+  xmonad $ docks defaults
+
+defaults = desktopConfig {
+    terminal    = "alacritty",
+    modMask     = mod4Mask,
+    workspaces  = customWorkspaces,
+    layoutHook  = avoidStruts $ layoutHook desktopConfig
 }
