353c22a52ff59c67ad384edd3c4e2479c01fe55f
Eugene 'marengohue' Rebedailo <mokonamodokidesu@gmail.com>
2021-08-21 14:17:56 +0200

    Replace rust racer setup with rustic/lsp

--- src/home/__usrname__/.emacs.d/init.el
+++ src/home/__usrname__/.emacs.d/init.el
@@ -1,12 +1,13 @@
 (defun load-cfg (cfg-name)
     (load (expand-file-name cfg-name user-emacs-directory)))
 
+(load-cfg "keys")
 (load-cfg "style")
 (load-cfg "modeline")
 (load-cfg "indent")
 (load-cfg "package")
 (load-cfg "counsel")
-(load-cfg "rust-mode")
+(load-cfg "autocomplete")
 (load-cfg "which-key")
 (load-cfg "projectile")
 
@@ -18,7 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ivy-rich rich-ivy rainbow-delimiters which-ley projectile mood-line company racer rust-mode use-package counsel)))
+   '(rustic flycheck lsp-ui lsp-mode ivy-rich rich-ivy rainbow-delimiters which-ley projectile mood-line company use-package counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
